01bc635984b21c60362bae5a867777daa3ba0029
DMDZ99 <ldhdo430@naver.com>
2025-07-14 19:32:19 +0900

    unit

---
+++ TextRPG/TextRPG/Unit.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG
+{
+    public class Unit
+    {
+        public string Name;
+        public int Atk;
+        public int Def;
+        public int HP;
+        public int Level;
+        public int Mp;
+
+
+        public class Character : Unit
+        {
+            public float Exp;
+            public string Job;
+            public string Inventory;
+            public int Gold;
+
+            Items <Inventory>
+        }
+
+        public class Monster : Unit
+        {
+            public string DropItem;
+            public int DropExp;
+            public int DropGold;
+        }
+    }
+}
