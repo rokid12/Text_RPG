78d1e988072480f0e2580b7530e547f9b87e8c50
DMDZ99 <ldhdo430@naver.com>
2025-07-14 20:19:29 +0900

    monster_Array

--- TextRPG/TextRPG/Unit.cs
+++ TextRPG/TextRPG/Unit.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TextRPG;
 
 namespace TextRPG
 {
@@ -15,16 +16,16 @@ namespace TextRPG
         public int HP;
         public int Level;
         public int Mp;
+    }
 
-
-        public class Character : Unit
+    public class Character : Unit
         {
-            public float Exp;
+            public int Exp;
             public string Job;
             public string Inventory;
             public int Gold;
 
-            Items <Inventory>
+            public static List<Inventory> item = new List<Inventory>();
         }
 
         public class Monster : Unit
@@ -32,6 +33,32 @@ namespace TextRPG
             public string DropItem;
             public int DropExp;
             public int DropGold;
+
+            // 몬스터 배열
+            public static Monster[] MonsterArray;
+
+            // 생성자
+            public Monster(string name, int atk, int def, int hp, int level, int mp, string dropItem, int dropExp, int dropGold)
+            {
+                Name = name;
+                Atk = atk;
+                Def = def;
+                HP = hp;
+                Level = level;
+                Mp = mp;
+                DropItem = dropItem;
+                DropExp = dropExp;
+                DropGold = dropGold;
+
+            }
+            
+            public static void MonsterInfo()
+            {
+            MonsterArray = new Monster[]
+        {
+            new Monster("미니언", 5, 0, 15, 2, 10, "", 2, 5),
+            new Monster("공허충", 9, 2, 10, 3, 10, "", 3, 10),
+            new Monster("대포미니언", 8, 5, 25, 5, 20, "", 5, 20)
+        };
         }
-    }
 }
