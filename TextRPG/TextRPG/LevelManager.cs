fd381e625c04cf9a9931baa107ab779e3468127b
sulg16 <mmopq2@naver.com>
2025-07-15 20:51:58 +0900

    Update LevelManager.cs
    오류없는버전(테스트x)

--- TextRPG/TextRPG/LevelManager.cs
+++ TextRPG/TextRPG/LevelManager.cs
@@ -1,20 +1,19 @@
+using System;
+using TextRPG;
+
 class LevelManager
 {
-    private Unit unit;
+    private Character character;
 
-    public LevelManager(Unit unit)
+    public LevelManager(Character character)
     {
-        this.unit = unit;
+        this.character = character;
     }
 
     public void AddExperience(int exp)
     {
-       
-    }
-
-  
-    private void LevelUp()
-    {
-        
+        character.exp += exp; // 경험치 누적
+        Console.WriteLine($"{character.name}이(가) 경험치 {exp}을 얻었습니다.");
+        character.LevelUp(); // unit에 있는 LevelUp 호출?
     }
 }
