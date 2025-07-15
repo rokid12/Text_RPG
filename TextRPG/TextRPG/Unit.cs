fafde77f79c0a5b2c59e23e11694a8e952620f3f
DMDZ99 <ldhdo430@naver.com>
2025-07-15 09:34:37 +0900

    Unit2

--- TextRPG/TextRPG/Unit.cs
+++ TextRPG/TextRPG/Unit.cs
@@ -14,11 +14,38 @@ namespace TextRPG
         public int Atk;
         public int Def;
         public int HP;
-        public int Level;
         public int Mp;
+        public int Level;
+
+
+        //생성자
+        public Unit(string name, int atk, int def, int hp, int mp, int level)
+        {
+            Name = name;
+            Atk = atk;
+            Def = def;
+            HP = hp;
+            Mp = mp;
+            Level = level;
+        }
+
+        //공격
+        public void Attack(Unit target)
+        {
+            Console.WriteLine($"{Name}이(가) {target.Name}을(를) 공격하였습니다.");
+            target.TakeDamage(Atk);
+        }
+
+        //피격
+        public void TakeDamage(int atk)
+        {
+            TakeDamage = Atk - Def;
+            Console.WriteLine($"{Name}이(가) {TakeDamage} 데미지를 입었습니다.")
+        }
+
     }
 
-    public class Character : Unit
+        public class Character : Unit
         {
             public int Exp;
             public string Job;
@@ -26,6 +53,27 @@ namespace TextRPG
             public int Gold;
 
             public static List<Inventory> item = new List<Inventory>();
+
+            public Character(string name, int atk, int def, int hp, int mp, int level, int exp, string job, int gold)
+            :base(name, atk, def, hp, mp, level)   
+            {
+                Exp = exp;
+                Job = job;
+                Gold = gold;
+            }
+
+            public void LevelUp()
+            {   //레벨당 경험치가 가득찼을때
+                if(Exp = )
+                {
+                    Level++;
+                    Atk = +2;
+                    Def = 
+                    HP =
+                    Mp =
+                    Exp = 0;
+                }
+            }
         }
 
         public class Monster : Unit
@@ -38,27 +86,22 @@ namespace TextRPG
             public static Monster[] MonsterArray;
 
             // 생성자
-            public Monster(string name, int atk, int def, int hp, int level, int mp, string dropItem, int dropExp, int dropGold)
+            public Monster(string name, int atk, int def, int hp, int mp, int level, string dropItem, int dropExp, int dropGold)
+            : base(name, atk, def, hp, mp, level)
             {
-                Name = name;
-                Atk = atk;
-                Def = def;
-                HP = hp;
-                Level = level;
-                Mp = mp;
                 DropItem = dropItem;
                 DropExp = dropExp;
                 DropGold = dropGold;
-
             }
-            
+
             public static void MonsterInfo()
             {
-            MonsterArray = new Monster[]
-        {
-            new Monster("미니언", 5, 0, 15, 2, 10, "", 2, 5),
-            new Monster("공허충", 9, 2, 10, 3, 10, "", 3, 10),
-            new Monster("대포미니언", 8, 5, 25, 5, 20, "", 5, 20)
-        };
+                MonsterArray = new Monster[]
+                {
+                    new Monster("미니언", 5, 0, 15, 2, 10, "", 2, 5),
+                    new Monster("공허충", 9, 2, 10, 3, 10, "", 3, 10),
+                    new Monster("대포미니언", 8, 5, 25, 5, 20, "", 5, 20)
+                };
+            }
         }
 }
