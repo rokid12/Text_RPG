2e70f3030326f7490fced78b629d6f571abb3024
KimNamJin <48212042+UserJin@users.noreply.github.com>
2025-07-15 16:27:43 +0900

    배틀에서 사용한 클래스 주석처리

--- TextRPG/TextRPG/BattleSystem/Battle.cs
+++ TextRPG/TextRPG/BattleSystem/Battle.cs
@@ -190,92 +190,92 @@ namespace TextRPG.BattleSystem
 
 // 아래 클래스들은 일단 임시로 사용한 클래스
 
-public class Unit
-{
-    public string Name;
-    public int Atk;
-    public int Def;
-    public int HP;
-    public int Mp;
-    public int Level;
+//public class Unit
+//{
+//    public string Name;
+//    public int Atk;
+//    public int Def;
+//    public int HP;
+//    public int Mp;
+//    public int Level;
 
 
-    //생성자
-    public Unit(string name, int atk, int def, int hp, int mp, int level)
-    {
-        Name = name;
-        Atk = atk;
-        Def = def;
-        HP = hp;
-        Mp = mp;
-        Level = level;
-    }
+//    //생성자
+//    public Unit(string name, int atk, int def, int hp, int mp, int level)
+//    {
+//        Name = name;
+//        Atk = atk;
+//        Def = def;
+//        HP = hp;
+//        Mp = mp;
+//        Level = level;
+//    }
 
-    //공격
-    public void Attack(Unit target)
-    {
-        Console.WriteLine($"{Name}이(가) {target.Name}을(를) 공격하였습니다.");
-        target.TakeDamage(Atk);
-    }
+//    //공격
+//    public void Attack(Unit target)
+//    {
+//        Console.WriteLine($"{Name}이(가) {target.Name}을(를) 공격하였습니다.");
+//        target.TakeDamage(Atk);
+//    }
 
-    //피격
-    public void TakeDamage(int atk)
-    {
-        int TakeDamage = atk - Def;
-        HP -= TakeDamage;
-        Console.WriteLine($"{Name}이(가) {TakeDamage} 데미지를 입었습니다.");
-    }
+//    //피격
+//    public void TakeDamage(int atk)
+//    {
+//        int TakeDamage = atk - Def;
+//        HP -= TakeDamage;
+//        Console.WriteLine($"{Name}이(가) {TakeDamage} 데미지를 입었습니다.");
+//    }
 
-}
+//}
 
-public class Character : Unit
-{
-    public int Exp;
-    public string Job;
-    public string Inventory;
-    public int Gold;
+//public class Character : Unit
+//{
+//    public int Exp;
+//    public string Job;
+//    public string Inventory;
+//    public int Gold;
 
-    //public static List<Inventory> item = new List<Inventory>();
+//    //public static List<Inventory> item = new List<Inventory>();
 
-    public Character(string name, int atk, int def, int hp, int mp, int level, int exp, string job, int gold)
-    : base(name, atk, def, hp, mp, level)
-    {
-        Exp = exp;
-        Job = job;
-        Gold = gold;
-    }
+//    public Character(string name, int atk, int def, int hp, int mp, int level, int exp, string job, int gold)
+//    : base(name, atk, def, hp, mp, level)
+//    {
+//        Exp = exp;
+//        Job = job;
+//        Gold = gold;
+//    }
 
-    public void LevelUp()
-    {   //레벨당 경험치가 가득찼을때
+//    public void LevelUp()
+//    {   //레벨당 경험치가 가득찼을때
 
-    }
-}
+//    }
+//}
 
-public class Monster : Unit
-{
-    public string DropItem;
-    public int DropExp;
-    public int DropGold;
+//public class Monster : Unit
+//{
+//    public string DropItem;
+//    public int DropExp;
+//    public int DropGold;
 
-    // 몬스터 배열
-    public static Monster[] MonsterArray;
+//    // 몬스터 배열
+//    public static Monster[] MonsterArray;
 
-    // 생성자
-    public Monster(string name, int atk, int def, int hp, int mp, int level, string dropItem, int dropExp, int dropGold)
-    : base(name, atk, def, hp, mp, level)
-    {
-        DropItem = dropItem;
-        DropExp = dropExp;
-        DropGold = dropGold;
-    }
+//    // 생성자
+//    public Monster(string name, int atk, int def, int hp, int mp, int level, string dropItem, int dropExp, int dropGold)
+//    : base(name, atk, def, hp, mp, level)
+//    {
+//        DropItem = dropItem;
+//        DropExp = dropExp;
+//        DropGold = dropGold;
+//    }
 
-    public static void MonsterInfo()
-    {
-        MonsterArray = new Monster[]
-        {
-                    new Monster("미니언", 5, 0, 15, 2, 10, "", 2, 5),
-                    new Monster("공허충", 9, 2, 10, 3, 10, "", 3, 10),
-                    new Monster("대포미니언", 8, 5, 25, 5, 20, "", 5, 20)
-        };
-    }
-}
+//    public static void MonsterInfo()
+//    {
+//        MonsterArray = new Monster[]
+//        {
+//                    new Monster("미니언", 5, 0, 15, 2, 10, "", 2, 5),
+//                    new Monster("공허충", 9, 2, 10, 3, 10, "", 3, 10),
+//                    new Monster("대포미니언", 8, 5, 25, 5, 20, "", 5, 20)
+//        };
+//    }
+//}
