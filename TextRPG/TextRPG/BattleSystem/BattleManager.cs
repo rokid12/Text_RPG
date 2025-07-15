358718da4ab6eb81a6959908cb2011bef7c237a0
KimNamJin <48212042+UserJin@users.noreply.github.com>
2025-07-15 17:16:28 +0900

    전투 기능 확인 함수 추가
    배틀이 제대로 되는 지 확인하기 위한 함수
    그냥 메인에서 Battle.Instance.DebugBattleSystem(); 복붙해서 쓰면 전투 기능 확인 가능함

--- TextRPG/TextRPG/BattleSystem/BattleManager.cs
+++ TextRPG/TextRPG/BattleSystem/BattleManager.cs
@@ -35,5 +35,21 @@ namespace TextRPG.BattleSystem
             Battle battle = new Battle(allies, enemies);
             battle.ExecuteBattle();
         }
+
+        // 배틀이 제대로 되는 지 확인하기 위한 함수
+        // 그냥 메인에서 Battle.Instance.DebugBattleSystem(); 복붙해서 쓰면 전투 기능 확인 가능함
+        public void DebugBattleSystem()
+        {
+            List<Character> characters = new List<Character>();
+            List<Monster> monsters = new List<Monster>();
+
+            characters.Add(new Character("르탄", 10, 5, 100, 100, 1, 0, "전사", 1000));
+            monsters.Add(new Monster("공허충", 2, 3, 20, 0, 1, "", 5, 100));
+            monsters.Add(new Monster("공허충", 2, 3, 20, 0, 1, "", 5, 100));
+            monsters.Add(new Monster("공허충", 2, 3, 20, 0, 1, "", 5, 100));
+
+            Battle battle = new Battle(characters, monsters);
+            battle.ExecuteBattle();
+        }
     }
 }
