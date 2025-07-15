d9dcaa5d1ee160650a8ab12bb692ab9428b424aa
KO <rokid_@naver.com>
2025-07-15 16:48:44 +0900

    //
    //

---
+++ TextRPG/TextRPG/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG
+{
+    internal class GameManager
+    {
+        public Character player = new Character("rtan", 5, 5, 100, 100, 1, 0, "전사", 1000);
+    }
+}
