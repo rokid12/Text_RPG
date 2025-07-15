b0700d2ee8c66a91f247887178b5575830ad7033
sulg16 <mmopq2@naver.com>
2025-07-15 19:35:31 +0900

    Unit 불러오기
    불러오기 만 됨

--- TextRPG/TextRPG/LevelManager.cs
+++ TextRPG/TextRPG/LevelManager.cs
@@ -1,35 +1,20 @@
-﻿
-public class LevelManager
+class LevelManager
 {
-    /*
-    private Player player;
+    private Unit unit;
 
-    public LevelManager(Player player)
+    public LevelManager(Unit unit)
     {
-        this.player = player;
-    }
-    */
-
-    public void Experience(int exp)
-    {
-        Console.WriteLine($"경험치 {exp}을(를) 획득했습니다!");
-        player.Experience += exp;
-
-        while (player.Experience >= player.MaxExperience)
-        {
-            player.Experience -= player.MaxExperience;
-            LevelUp();
-        }
+        this.unit = unit;
     }
 
+    public void AddExperience(int exp)
+    {
+       
+    }
+
+  
     private void LevelUp()
     {
-        player.Level++;
-        player.MaxExperience += 50;
-        player.Attack += 2;
-
-        Console.WriteLine($"레벨업! 현재 레벨은 {player.Level} 입니다!");
-        Console.WriteLine($"공격력이 {player.Attack}로 증가했습니다!");
+        
     }
 }
-
