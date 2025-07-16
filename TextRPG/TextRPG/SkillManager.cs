0bd772cddba953dac50dc95da43149fb01a1753b
DMDZ99 <ldhdo430@naver.com>
2025-07-16 15:06:52 +0900

    Update SkillManager.cs

--- TextRPG/TextRPG/SkillManager.cs
+++ TextRPG/TextRPG/SkillManager.cs
@@ -9,7 +9,7 @@ namespace TextRPG
     public static class SkillManager
     {
         public static Skill bite = new Skill("물기", 10, 15);
-        public static Skill Cannon = new Skill("대포발사", 20, 40);
+        public static Skill cannon = new Skill("대포발사", 20, 40);
         public static Skill counterattack = new Skill("반격", 15, 50 );
         public static Skill javelin = new Skill("투창", 10, 30);
         public static Skill raisethorn = new Skill("가시 돋히기", 5, 20);
