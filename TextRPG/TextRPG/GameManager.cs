f0d41565520ab0be4e1bd9d5b10b0bf06434b0a9
KimNamJin <48212042+UserJin@users.noreply.github.com>
2025-07-17 19:26:04 +0900

    캐릭터 생성 & 직업 선택

--- TextRPG/TextRPG/GameManager.cs
+++ TextRPG/TextRPG/GameManager.cs
@@ -24,10 +24,37 @@ namespace TextRPG
 
         private GameManager()
         {
-            player = new Character("rtan", 5, 5, 100, 100, 1, 0, "전사", 1000);
+            CharacterMaking();
             dungeon = new Dungeon(player);
         }
 
         public Dungeon GetDungeon() => dungeon;
+
+        private void CharacterMaking()
+        {
+            Console.Write("이름을 입력해주세요.\n>> ");
+            string name = Console.ReadLine();
+
+            // 직업 선택?
+            Console.WriteLine("\n1. 전사\n2. 궁수\n3. 마법사\n");
+            //Console.Write("\n직업을 선택해주세요.\n>> ");
+
+            int choice = InputManager.PickNumber(3, 1);
+            switch(choice)
+            {
+                case 1:
+                    player = new Character(name, JobData.Warrior._atk, JobData.Warrior._def, JobData.Warrior._maxHp, JobData.Warrior._maxMp, 1, 0, JobData.Warrior._jobName, 1000);
+                    break;
+                case 2:
+                    player = new Character(name, JobData.Archor._atk, JobData.Archor._def, JobData.Archor._maxHp, JobData.Archor._maxMp, 1, 0, JobData.Archor._jobName, 1000);
+                    break;
+                case 3:
+                    player = new Character(name, JobData.Mage._atk, JobData.Mage._def, JobData.Mage._maxHp, JobData.Mage._maxMp, 1, 0, JobData.Mage._jobName, 1000);
+                    break;
+            }
+
+            Console.WriteLine("\n캐릭터 생성에 성공하였습니다!\n계속하시려면 아무 키나 눌러주세요...");
+            Console.ReadKey();
+        }
     }
 }
---
+++ TextRPG/TextRPG/JobData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG
+{
+    class JobData
+    {
+        public static Job Warrior = new Job(5, 5, 100, 60, "전사");
+        public static Job Archor = new Job(7, 4, 80, 80, "궁수");
+        public static Job Mage = new Job(10, 2, 60, 150, "마법사");
+    }
+
+    struct Job
+    {
+        public int _atk;
+        public int _def;
+        public int _maxHp;
+        public int _maxMp;
+        public string _jobName;
+
+        public Job(int atk, int def, int maxHp, int maxMp, string jobName)
+        {
+            _atk = atk;
+            _def = def;
+            _maxHp = maxHp;
+            _maxMp = maxMp;
+            _jobName = jobName;
+        }
+    }
+}
