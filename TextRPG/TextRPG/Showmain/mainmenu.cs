61b302aa1e6bde828d5be609a74d7016f38b0c92
KimNamJin <48212042+UserJin@users.noreply.github.com>
2025-07-18 09:54:16 +0900

    인트로 먼저 보여주기

--- TextRPG/TextRPG/Showmain/mainmenu.cs
+++ TextRPG/TextRPG/Showmain/mainmenu.cs
@@ -6,6 +6,10 @@ namespace TextRPG
     {
         static void Main(string[] args)
         {
+            UIManager.Instance.ShowIntro();
+            Console.WriteLine("\n\n시작하시려면 아무키나 입력하세요...");
+            Console.ReadKey();
+
             var getInven = GameManager.Instance.player.GetInventory();
 
             ItemEquipable trinityForce = new ItemEquipable(ItemData.Instance.trinityForce); // 이런식으로 복사해서 넣으면 될듯?
@@ -13,7 +17,6 @@ namespace TextRPG
 
             GameManager.Instance.player.GetInventory().Add(ItemData.Instance.potion);
 
-            UIManager.Instance.ShowIntro();
             UIManager.Instance.ShowMainMenu();
         }
     }
